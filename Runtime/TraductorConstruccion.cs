a92a0b9b57ba082154e68aa58f974ee5ac8278dd
Ging1991 <carloscaballeromorel@gmail.com>
2026-08-27 14:15:40 -0300

    Fix idioma ingles

--- Runtime/TraductorConstruccion.cs
+++ Runtime/TraductorConstruccion.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Bounds.Contruccion;
 using Ging1991.Idiomas;
 using UnityEngine;
@@ -8,7 +9,7 @@ namespace Bounds.Construccion {
 
 		public VisorConstruccion visorConstruccion;
 
-		public string Traducir(string clave, string traduccionParcial) {
+		public string Traducir(string clave, string traduccionParcial, List<string> opciones) {
 			if (clave == "VENDER_PRECIO")
 				return traduccionParcial.Replace("[PRECIO]", $"{visorConstruccion.CalcularPrecioActual()}");
 			throw new System.NotImplementedException();
